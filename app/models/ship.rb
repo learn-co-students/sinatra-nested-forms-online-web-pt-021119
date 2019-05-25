dc01ed29a02f2f662a44e2ba2f41c7cb449adfcd
Berlinta <nashfft@gmail.com>
2019-05-24 19:55:20 -0700

    test

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+
+attr_accessor :name, :type, :booty   
+
+  SHIPS = []  
+
+  def initiazlie
+    SHIPS << self
+  end 
+ 
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
+
 end
