f2e072ae5880e3f7833b94b698ddadba8751ac24
luizfper <luizfper@gmail.com>
2019-05-26 19:39:12 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+	attr_accessor :name, :type, :booty
+
+	@@ships=[]
+
+	def initialize(attribs)
+		attribs.each do |k,v|
+			self.send("#{k}=",v)
+		end
+		@@ships<<self
+	end
+
+	def self.all
+		@@ships
+	end
+
+	def self.clear
+		@@ships.clear
+	end
+
 end
