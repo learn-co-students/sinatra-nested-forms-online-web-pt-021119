f373ecc2557cef4c2d7bcc170d965f38177fc60a
lieby weintraub <liebahinda@gmail.com>
2019-05-29 18:36:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,8 +12,8 @@ module FormsLab
   
  post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
-      params[:pirate][:ships].each do |hash|
-        Ship.new(hash)
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
       end
       @ships = Ship.all
 
